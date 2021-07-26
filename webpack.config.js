3846a5bc174ea20f8431c417396a84a259f2f125
Kayla Fortin <kayla.hennig@gmail.com>
2021-07-25 20:11:53 -0400

    template html

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true
-  },
-};
-
-module.exports = config;
+module.exports = function (env, { mode }) {
+  const isEnvDevelopment = mode === 'development';
+  console.log('here', mode)
+  return {
+    entry: './src/index.js',
+    watch: isEnvDevelopment,
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      })
+    ],
+  }
+}
